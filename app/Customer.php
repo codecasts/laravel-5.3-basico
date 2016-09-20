d2102b744082acad992c5bdb1361d87d45171012
Fabio Vedovelli <vedovelli@gmail.com>
2016-09-20 06:56:12 -0300

    Aulas sobre Model

--- app/Customer.php
+++ app/Customer.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+
+    protected $fillable = ['name', 'birthdate', 'special_customer', 'city', 'state'];
+
+    protected $casts = [
+        'special_customer' => 'boolean',
+        'birthdate' => 'date',
+    ];
+
+    public function purchases()
+    {
+        return $this->hasMany(Puchases::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,35 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    // LISTAR CUSTOMERS
+    // $customers = Charlie\Customer::paginate(10);
+    // dd($customers->toArray());
+    // echo $customers->render();
+
+    // CRIAR NOVO CUSTOMER
+    $data = [
+        'name' => 'Fabio Vedovelli',
+        'city' => 'Santo Andre',
+        'state' => 'SP',
+        'special_customer' => true,
+        'birthdate' => '1974-08-07 00:00:00'
+    ];
+    $customer = Charlie\Customer::create($data);
+    dd($customer->toArray());
+
+    // TRAZER CUSTOMER ID 2
+
+    $data = [
+        'name' => 'Um novo cara',
+        'birthdate' => '1989-01-01 00:00:00',
+    ];
+
+    $customer = Charlie\Customer::find(2);
+
+    $customer->name = 'Um outro cara';
+    // $customer->save();
+
+    $customer->update($data);
+
+
 });
